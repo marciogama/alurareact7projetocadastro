fb864a9a4565b83dfefc2227da7573bccb92898c
marciogama <marciogama2000@gmail.com>
2020-11-25 19:17:45 -0300

    Hooks customizados e compartilhando código entre componentes

--- src/components/FormularioCadastro/DadosPessoais.jsx
+++ src/components/FormularioCadastro/DadosPessoais.jsx
@@ -1,41 +1,25 @@
 import React, { useState, useContext } from "react";
 import { TextField, Button, Switch, FormControlLabel } from "@material-ui/core";
 import ValidacoesCadastro from "../../contexts/ValicacoesCadastro";
+import useErros from "../../hooks/useErros";
 
-function DadosPessoais({aoEnviar}) {
+function DadosPessoais({ aoEnviar }) {
   const [nome, setNome] = useState("");
   const [sobrenome, setSobrenome] = useState("");
   const [cpf, setCpf] = useState("");
   const [promocoes, setPromocoes] = useState(true);
   const [novidades, setNovidades] = useState(false);
-  const [erros, setErros] = useState({cpf:{valido:true, texto:""}, nome:{valido:true, texto:""}});
-
   const validacoes = useContext(ValidacoesCadastro);
-
-  function validarCampos(event) {
-      const {name, value} = event.target;
-      const novoEstado = {...erros};
-      novoEstado[name] = validacoes[name](value);
-      setErros(novoEstado);
-  }
-
-  function possoEnviar (){
-    for(let campo in erros){
-      if(!erros[campo].valido){
-        return false;
-      }
-    }
-    return true;
-  }
+  const [erros, validarCampos, possoEnviar] = useErros(validacoes);
 
   return (
     <form
       onSubmit={(event) => {
         event.preventDefault();
-        if(possoEnviar()){
-          aoEnviar({nome, sobrenome, cpf, promocoes, novidades})
+        if (possoEnviar()) {
+          aoEnviar({ nome, sobrenome, cpf, promocoes, novidades });
         }
-      }} 
+      }}
     >
       <TextField
         value={nome}
